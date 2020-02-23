f4c1272ee14b07799d7d6657df579190598d36bf
Shivam Baghel <shivamunlimited@gmail.com>
2020-02-23 23:27:19 +0530

    Source Commit

---
+++ src/personADT/ContactListADT.java
@@ -0,0 +1,11 @@
+package personADT;
+
+public interface ContactListADT<Person> {
+    boolean add(Person person);
+
+    boolean delete(int index);
+
+    void search(String name);
+
+    void viewAllContacts();
+}
